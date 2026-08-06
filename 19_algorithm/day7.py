c8bf118f956493143f2ec519619436920b8c9d16
castle0tiger <sb948412@gmail.com>
2026-08-06 13:42:51 +0900

    알고리즘 Day7: v2 효율 버전 추가 (찾자마자 return, result 리스트 제거)

--- 19_algorithm/day7.py
+++ 19_algorithm/day7.py
@@ -1,4 +1,4 @@
-def first_unique(text):
+def first_unique_v1(text):
     counts = {}
 
     for t in text:
@@ -13,11 +13,34 @@ def first_unique(text):
         if counts[t] == 1:
             result.append(t)
 
-    if result:
+    if result:               #리스트에 요소 존재 유무 조사
         return result[0]
     else:
         return None
 
-print(f"leetcode ->  {first_unique('leetcode')}")
-print(f"aabbcc ->  {first_unique('aabbcc')}")
-print(f"abcabd ->  {first_unique('abcabd')}")
+print(f"leetcode ->  {first_unique_v1('leetcode')}")
+print(f"aabbcc ->  {first_unique_v1('aabbcc')}")
+print(f"abcabd ->  {first_unique_v1('abcabd')}")
+print()
+
+
+## 실무 피드백 _ 코드 효율화 
+
+def first_unique_v2(text):
+    counts = {}
+
+    for t in text:
+        if t in counts:
+            counts[t] += 1
+        else:
+            counts[t] = 1
+
+    for t in text:
+        if counts[t] == 1:
+            return t
+    return None
+
+
+print(f"leetcode ->  {first_unique_v2('leetcode')}")
+print(f"aabbcc ->  {first_unique_v2('aabbcc')}")
+print(f"abcabd ->  {first_unique_v2('abcabd')}")
