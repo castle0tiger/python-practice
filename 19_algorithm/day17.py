f21ea6c4f23a3800d69612b8984d78cdeb26deff
castle0tiger <sb948412@gmail.com>
2026-08-16 12:24:20 +0900

    알고리즘 Day17: 길이별 단어 묶기 (그룹핑=값이 리스트인 딕셔너리, Day4 카운터의 진화)

---
+++ 19_algorithm/day17.py
@@ -0,0 +1,15 @@
+def group_by_length(texts):
+    groups = {}
+
+    for t in texts:
+        if len(t) in groups:
+            groups[len(t)].append(t)
+        else:
+            groups[len(t)] = [t]
+
+    return groups 
+
+            
+print(f"['a', 'bb', 'cc', 'ddd'] -> {group_by_length(['a', 'bb', 'cc', 'ddd'])}")
+print(f"['cat', 'dog', 'hi', 'a'] -> {group_by_length(['cat', 'dog', 'hi', 'a'])}")
+print("")
