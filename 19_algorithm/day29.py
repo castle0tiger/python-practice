90921f2c6b9ac76fea469cc054171ea7b554c97a
castle0tiger <sb948412@gmail.com>
2026-09-01 14:30:22 +0900

    Day29: 방법2 완성 (max(flatten()) 역할분리) + 설계 원칙 교훈

--- 19_algorithm/day29.py
+++ 19_algorithm/day29.py
@@ -18,20 +18,36 @@ print(nested_max([[[7]]]))
 print("")
 
 
-## 평탄화한 후 max()로 최대값 추출
-def nested_max_v2(input):
+## 평탄화한 후 바깥에서 max()로 최대값 추출
+def flatten(input):
     result = []
 
     for i in input:
         if isinstance(i, list):
-            result.extend(nested_max_v2(i))
+            result.extend(flatten(i))
         else:
             result.append(i)
 
     return result
+
+print(max(flatten([3, 1, 2])))
+print(max(flatten(([1, [2, 3], 4]))))
+print(max(flatten([1, [2, [9, 4]], 5])))
+print(max(flatten([[10, 2], [3, [4, 5]]])))
+print(max(flatten([[[7]]])))
+print("")
+
+
+## 평탄화 함수를 최대값 뽑는 함수로 감싸는 방법도 있음
+def nested_max_v2(input):
+    return max(flatten(input))   # 평탄화한 걸 max
 
 print(nested_max_v2([3, 1, 2]))
 print(nested_max_v2([1, [2, 3], 4]))
 print(nested_max_v2([1, [2, [9, 4]], 5]))
 print(nested_max_v2([[10, 2], [3, [4, 5]]]))
 print(nested_max_v2([[[7]]]))
+print("")
+
+## 교훈 _ 실무 설계 원칙
+# 설계가 어렵고 복잡하다면, "한 함수가 다 하려 하지 말고 나눠라"
